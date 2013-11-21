591b97a0719e63e7cbea26e427747a5ebe830c06
Quentin Morisseau <quentin.morisseau@gmail.com>
2013-11-21 18:00:10 +0100

    Modif conseillées

--- Integration/newstory/js/script.js
+++ Integration/newstory/js/script.js
@@ -2,30 +2,28 @@ var steps = 1;
 $(".createContainer a.newStep").on("click", function(e){
     e.preventDefault();
     steps++;
-    
-    
-    
-    /******************************* PAS D'ID SUR LES INPUT ET AUTRES CONNERIES, UNE FONCTION AU CLIC SUR LES LABEL QUI FOCUS L'INPUT SUIVANT ! */
-    
-    
     var section = $("<fieldset>").addClass("innerStep");
     //Separator
     var hr = $("<hr>");
     //Cur step
     var step = $("<span>").addClass("curStep").html(steps);
     //Title
-    var labelTitle = $("<label>").attr("for","step"+steps+"Title").html("Title");
-    var inputTitle = $("<input>").attr("type","text").attr("id","step"+steps+"Title");
+    var labelTitle = $("<label>").html("Title");
+    var inputTitle = $("<input>").attr("type","text").attr("name","step"+steps+"Title");
     //Description
-    var labelDesc = $("<label>").attr("for","step"+steps+"Desc").html("Description");
-    var txtDesc = $("<textarea>").attr("id","step"+steps+"Desc");
+    var labelDesc = $("<label>").html("Description");
+    var txtDesc = $("<textarea>").attr("name","step"+steps+"Desc");
     //Location
-    var labelLoc = $("<label>").attr("for","step"+steps+"Location").html("Location");
-    var inputLoc = $("<input>").attr("type","text").attr("id","step"+steps+"Location");
+    var labelLoc = $("<label>").html("Location");
+    var inputLoc = $("<input>").attr("type","text").attr("name","step"+steps+"Location");
     //File
-    var labelFile = $("<label>").attr("for","step"+steps+"File").html("Choose File");
-    var inputFile = $("<input>").attr("type","file").attr("id","step"+steps+"File");
+    var labelFile = $("<label>").html("Choose File");
+    var inputFile = $("<input>").attr("type","file").attr("name","step"+steps+"File");
     
     section.append(hr, step, labelTitle, inputTitle, labelDesc, txtDesc, labelLoc, inputLoc, labelFile, inputFile);
     $(".innerStep").last().after(section);
+});
+
+$("fieldset.innerStep").on("click", 'label', function(){
+    $(this).next().focus();
 });
